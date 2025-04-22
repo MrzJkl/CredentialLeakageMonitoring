796277c8cb227d70e46b253bf4a9bfc957bd07c6
Moritz Jökel <moritz@feuersoftware.com>
2025-04-22 18:40:08 +0200

    .

--- CredentialLeakageMonitoring/Services/CryptoService.cs
+++ CredentialLeakageMonitoring/Services/CryptoService.cs
@@ -1,7 +1,16 @@
-﻿namespace CredentialLeakageMonitoring.Services
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace CredentialLeakageMonitoring.Services
 {
     public class CryptoService
     {
+        public byte[] HashEmail(string email)
+        {
+            byte[] inputBytes = Encoding.UTF8.GetBytes(email);
+            return SHA3_512.HashData(inputBytes);
+        }
+
 
     }
 }
