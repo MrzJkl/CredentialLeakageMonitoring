b3ae024db51680928e7cf03d9581f005b69b4c9e
Moritz Jökel <moritz@feuersoftware.com>
2025-04-22 16:10:21 +0200

    Add project files.

---
+++ CredentialLeakageMonitoring/Models/IngestionLeak.cs
@@ -0,0 +1,9 @@
+﻿namespace CredentialLeakageMonitoring.Models
+{
+    public record IngestionLeak
+    {
+        public string EMail { get; private set; }
+
+        public string Password { get; private set; }
+    }
+}
